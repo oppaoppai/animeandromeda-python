c136d71e935a14236e90c58eff31a42cc4b395c1
oppaoppai <marco.piovanello@gmail.com>
2020-09-02 14:26:09 +0200

    mixed list comparator

--- helpers.py
+++ helpers.py
@@ -5,16 +5,23 @@ def compareJSONdate(jsondate):
     date = str(jsondate)
     if "Z" in date:
         return datetime.strptime(
-            date, "%Y-%m-%dT%H:%M:%S.%fZ")
+            date, "%Y-%m-%dT%H:%M:%S.%fZ")  # is json date
     elif date == "None":
-        return datetime(1, 1, 1, 0, 0)
+        return datetime(1, 1, 1, 0, 0)     # isn't either json or timezone dateobj
     else:
         return datetime.strptime(
-            date, "%Y-%m-%d %H:%M:%S.%f")
+            date, "%Y-%m-%d %H:%M:%S.%f")  # has timezone
 
 
 def convertEpisode(ep):
+    # if episode is a special (multiple episodes in a single one)
     if "-" in ep:
-        return int(ep.split("-")[1])
+        parsed = int(ep.split("-")[0])
+        return (0, parsed, '')
     else:
-        return int(ep)
+        # mixed list comparator
+        try:
+            parsed = int(ep)
+            return (0, parsed, '')
+        except ValueError:
+            return (1, ep, '')
